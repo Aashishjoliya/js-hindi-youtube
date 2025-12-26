ac5c453f11edc0b8649a245eeb84aeebf885cebd
Aashishjoliya <aashish_ug_24@cse.nits.ac.in>
2025-12-26 09:40:11 +0000

    datatypes

---
+++ 01_basic/01_varible.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="hitesh@Google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2 //not allowed
+//accountId=2
+accountEmail="aashu@,com"
+accountPassword="54321"
+accountCity="Delhi"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,19 @@
+"use Strict";   //treat all JS code as newer version like classes modules
+//alert(3+3)  //we are using node not browser
+console.log(3+3);
+console.log("Hitesh");
+let name="Aashish"
+let age=18
+let isLoggedIn=false
+
+
+
+//number=>2 to power 53
+//bigint
+//String =""
+//boolean=true or false                //null =>standAlone value   //undefined=>
+// symbol=>unique
+
+//object
+
+console.log(typeof undefined);
