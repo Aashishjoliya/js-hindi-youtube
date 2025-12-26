053c4324cec02f88bf7b9ab1c25283c161d1580e
Aashishjoliya <aashish_ug_24@cse.nits.ac.in>
2025-12-26 10:32:27 +0000

    ConversionOperations

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,44 @@
+/*    let score=undefined
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// '33'=>33
+//"33abc"=>nan not a number but it's type is still a number
+//true=>1; false=>0;
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>true 0=>false
+// if Empty string=> false
+//if String is not Empty=>true       for conversion of other variable to boolean
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
+*/
+
+
+//*************************Operations *****************************************/
+
+let value =3
+let negValue= -value
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2**2);
+let str1="hello"
+let str2="World"
+let str3= str1+   str2
+console.log(str3);
+console.log("1"+2)
+console.log("1"+2+2)    //if string is first thenn all value is considered as string 
+console.log(1+4+5+"2") // if string is not first then first wr add then we do string 
+console.log(1+2+"2")
